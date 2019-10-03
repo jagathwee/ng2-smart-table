c2898dad61b6741df83feba5f24b477a8eeb82f2
Jagath Weerasinghe <w@soliduslink.com>
2019-10-03 17:14:39 +0200

    rm page button. keep only pre and next

--- src/ng2-smart-table/components/pager/pager.component.ts
+++ src/ng2-smart-table/components/pager/pager.component.ts
@@ -10,27 +10,12 @@ import { DataSource } from '../../lib/data-source/data-source';
     <nav *ngIf="shouldShow()" class="ng2-smart-pagination-nav">
       <ul class="ng2-smart-pagination pagination">
         <li class="ng2-smart-page-item page-item" [ngClass]="{disabled: getPage() == 1}">
-          <a class="ng2-smart-page-link page-link" href="#"
-          (click)="getPage() == 1 ? false : paginate(1)" aria-label="First">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">First</span>
-          </a>
-        </li>
-        <li class="ng2-smart-page-item page-item" [ngClass]="{disabled: getPage() == 1}">
           <a class="ng2-smart-page-link page-link page-link-prev" href="#"
              (click)="getPage() == 1 ? false : prev()" aria-label="Prev">
             <span aria-hidden="true">&lt;</span>
             <span class="sr-only">Prev</span>
           </a>
         </li>
-        <li class="ng2-smart-page-item page-item"
-        [ngClass]="{active: getPage() == page}" *ngFor="let page of getPages()">
-          <span class="ng2-smart-page-link page-link"
-          *ngIf="getPage() == page">{{ page }} <span class="sr-only">(current)</span></span>
-          <a class="ng2-smart-page-link page-link" href="#"
-          (click)="paginate(page)" *ngIf="getPage() != page">{{ page }}</a>
-        </li>
-
         <li class="ng2-smart-page-item page-item"
             [ngClass]="{disabled: getPage() == getLast()}">
           <a class="ng2-smart-page-link page-link page-link-next" href="#"
@@ -40,12 +25,12 @@ import { DataSource } from '../../lib/data-source/data-source';
           </a>
         </li>
       </ul>
-      <div class="ng2-smart-pagination summary">
+      <li class="ng2-smart-pagination summary">
         &emsp;
         <span>{{ getPageStart() }}</span> &nbsp;-&nbsp;
-        <span>{{ getPageEnd() }}</span> &nbsp;/&nbsp;
+        <span>{{ getPageEnd() }}</span> &nbsp;of&nbsp;
         <span>{{ count }}</span>
-      </div>
+      </li>
     </nav>
     
     <nav *ngIf="perPageSelect && perPageSelect.length > 0" class="ng2-smart-pagination-per-page">
